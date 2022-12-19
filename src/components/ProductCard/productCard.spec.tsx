a5f46825b631a09493778f19621d9b6683505579
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-19 15:08:01 -0300

    feat: add test on product card component werify if have a promotion price and normal price if product is on promotion

--- src/components/ProductCard/productCard.spec.tsx
+++ src/components/ProductCard/productCard.spec.tsx
@@ -5,9 +5,9 @@ type Product = {
   category: string;
   price: number;
   inPromotion: {
-        discountInPercent: number;
-        price: number;
-      } | null;
+    discountInPercent: number;
+    price: number;
+  } | null;
   image_url: string;
 };
 
@@ -21,14 +21,34 @@ const ProductCard = ({ product }: ProductCardProps) => {
       <img src={product.image_url} alt="any_alt" data-testid="product-image" />
       <h1>{product.title}</h1>
       <h3>{product.category}</h3>
-      <span data-testid="product_price">
-        {new Intl.NumberFormat("pt-br", {
-          style: "currency",
-          currency: "BRL",
-        }).format(product.price)}
-      </span>
-      {product.inPromotion && (
+      {product.inPromotion ? (
+        <>
+          <div>
+            <div>
+              {new Intl.NumberFormat("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              }).format(
+                product.price -
+                  (product.price * product.inPromotion.discountInPercent) / 100
+              )}
+            </div>
+            <div data-testid="normal-product-price">
+              {new Intl.NumberFormat("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              }).format(product.price)}
+            </div>
+          </div>
           <span>{product.inPromotion?.discountInPercent}% off</span>
+        </>
+      ) : (
+        <span data-testid="product_price">
+          {new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          }).format(product.price)}
+        </span>
       )}
     </div>
   );
@@ -84,9 +104,29 @@ describe("Product Card Component", () => {
         price: 80,
       },
     };
-    const { getByText } = render(<ProductCard product={makeMockProduct(mockProduct)} />);
+    const { getByText } = render(
+      <ProductCard product={makeMockProduct(mockProduct)} />
+    );
 
     expect(getByText(/20% off/i)).toBeInTheDocument();
+  });
+  it("should have a promotion price and normal price if product is in promotion", () => {
+    const mockProduct: Product = {
+      title: "any_title",
+      price: 100,
+      category: "any_category",
+      image_url: "any_url",
+      inPromotion: {
+        discountInPercent: 20,
+        price: 80,
+      },
+    };
+    const { getByText, getByTestId } = render(
+      <ProductCard product={makeMockProduct(mockProduct)} />
+    );
+
+    expect(getByText("R$ 80,00")).toBeInTheDocument();
+    expect(getByTestId("normal-product-price")).toBeInTheDocument();
   });
   it("not should have a section have the percent of discount if product has not in promotion", () => {
     const { queryByText } = render(<ProductCard product={makeMockProduct()} />);
