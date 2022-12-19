56da87afb51ee8fc025beaa127e28418edd71564
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-19 14:39:00 -0300

    feat: add mock product factory function

--- src/components/ProductCard/productCard.spec.tsx
+++ src/components/ProductCard/productCard.spec.tsx
@@ -1,40 +1,49 @@
-import {render} from '@testing-library/react'
+import { render } from "@testing-library/react";
 
 type Product = {
-    title: string;
-    category: string;
-    price: number;
-    inPromotion: {
-        discountInPercent: number
-        price: number
-    } | false;
-    image_url: string;
-}
+  title: string;
+  category: string;
+  price: number;
+  inPromotion:
+    | {
+        discountInPercent: number;
+        price: number;
+      }
+    | false;
+  image_url: string;
+};
 
 interface ProductCardProps {
-    product: Product
+  product: Product;
 }
 
-const ProductCard = ({product}: ProductCardProps) => {
-    return (
-        <div>
-            <img src={product.image_url} alt="any_alt" data-testid='product-image'/>
-        </div>
-    )
-}
+const ProductCard = ({ product }: ProductCardProps) => {
+  return (
+    <div>
+      <img src={product.image_url} alt="any_alt" data-testid="product-image" />
+    </div>
+  );
+};
 
-describe('Product Card Component', () => {
-    it('should have a image of product', () => {
-        const mockProduct: Product = {
-            title: 'any_title',
-            price: 100,
-            category: 'sport',
-            image_url: 'any_url',
-            inPromotion: false
-        }
-        const {getByTestId} = render(<ProductCard product={mockProduct}/>)
+const makeMockProduct = (otherMockProduct?: Product): Product => {
+  if (otherMockProduct) {
+    return otherMockProduct;
+  }
 
-        expect(getByTestId('product-image')).toBeInTheDocument()
-        expect(getByTestId('product-image')).toHaveProperty('alt', 'any_alt')
-    })
-})
+  return {
+    title: "any_title",
+    price: 100,
+    category: "sport",
+    image_url: "any_url",
+    inPromotion: false,
+  };
+};
+
+describe("Product Card Component", () => {
+  it("should have a image of product", () => {
+    const { getByTestId } = render(<ProductCard product={makeMockProduct()} />);
+
+    expect(getByTestId("product-image")).toBeInTheDocument();
+    expect(getByTestId("product-image")).toHaveProperty("alt", "any_alt");
+  });
+});
