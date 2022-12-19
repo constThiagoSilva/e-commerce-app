50f61823dffc443b2239ea881ee5e627034e97b6
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-19 14:47:43 -0300

    feat: add test on product card component werify if have a price with brazilian currency format

--- src/components/ProductCard/productCard.spec.tsx
+++ src/components/ProductCard/productCard.spec.tsx
@@ -23,6 +23,7 @@ const ProductCard = ({ product }: ProductCardProps) => {
       <img src={product.image_url} alt="any_alt" data-testid="product-image" />
       <h1>{product.title}</h1>
       <h3>{product.category}</h3>
+      <span data-testid='product_price'>{new Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(product.price)}</span>
     </div>
   );
 };
@@ -57,5 +58,11 @@ describe("Product Card Component", () => {
     const { getByText } = render(<ProductCard product={makeMockProduct()} />);
 
     expect(getByText("any_category")).toBeInTheDocument();
+  })
+  it('should have an product category title', () => {
+    const { getByTestId } = render(<ProductCard product={makeMockProduct()} />);
+
+    expect(getByTestId("product_price")).toBeInTheDocument();
+    expect(getByTestId("product_price")).toHaveTextContent(`R$ ${makeMockProduct().price},00`)
   })
 });
