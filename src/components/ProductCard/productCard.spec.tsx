d71ca0cc7ca644d484d95fbfe534ccec57737f64
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-19 14:56:33 -0300

    feat: add test on product card component werify if have a discount percent if product has in promotion

--- src/components/ProductCard/productCard.spec.tsx
+++ src/components/ProductCard/productCard.spec.tsx
@@ -4,12 +4,10 @@ type Product = {
   title: string;
   category: string;
   price: number;
-  inPromotion:
-    | {
+  inPromotion: {
         discountInPercent: number;
         price: number;
-      }
-    | false;
+      } | null;
   image_url: string;
 };
 
@@ -23,7 +21,15 @@ const ProductCard = ({ product }: ProductCardProps) => {
       <img src={product.image_url} alt="any_alt" data-testid="product-image" />
       <h1>{product.title}</h1>
       <h3>{product.category}</h3>
-      <span data-testid='product_price'>{new Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(product.price)}</span>
+      <span data-testid="product_price">
+        {new Intl.NumberFormat("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        }).format(product.price)}
+      </span>
+      {product.inPromotion && (
+          <span>{product.inPromotion?.discountInPercent}% off</span>
+      )}
     </div>
   );
 };
@@ -38,7 +44,7 @@ const makeMockProduct = (otherMockProduct?: Product): Product => {
     price: 100,
     category: "any_category",
     image_url: "any_url",
-    inPromotion: false,
+    inPromotion: null,
   };
 };
 
@@ -49,20 +55,37 @@ describe("Product Card Component", () => {
     expect(getByTestId("product-image")).toBeInTheDocument();
     expect(getByTestId("product-image")).toHaveProperty("alt", "any_alt");
   });
-  it('should have an product title', () => {
+  it("should have an product title", () => {
     const { getByText } = render(<ProductCard product={makeMockProduct()} />);
 
     expect(getByText("any_title")).toBeInTheDocument();
-  })
-  it('should have an product category title', () => {
+  });
+  it("should have an product category title", () => {
     const { getByText } = render(<ProductCard product={makeMockProduct()} />);
 
     expect(getByText("any_category")).toBeInTheDocument();
-  })
-  it('should have an product category title', () => {
+  });
+  it("should have an product category title", () => {
     const { getByTestId } = render(<ProductCard product={makeMockProduct()} />);
 
     expect(getByTestId("product_price")).toBeInTheDocument();
-    expect(getByTestId("product_price")).toHaveTextContent(`R$ ${makeMockProduct().price},00`)
-  })
+    expect(getByTestId("product_price")).toHaveTextContent(
+      `R$ ${makeMockProduct().price},00`
+    );
+  });
+  it("should have a section have the percent of discount if product has in promotion", () => {
+    const mockProduct: Product = {
+      title: "any_title",
+      price: 100,
+      category: "any_category",
+      image_url: "any_url",
+      inPromotion: {
+        discountInPercent: 20,
+        price: 80,
+      },
+    };
+    const { getByText } = render(<ProductCard product={makeMockProduct(mockProduct)} />);
+
+    expect(getByText(/20% off/i)).toBeInTheDocument();
+  });
 });
