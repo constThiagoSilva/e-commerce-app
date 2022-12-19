e4c3628ad4c90dd5be22ff9624a7cdfb8105d048
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-19 19:20:08 -0300

    feat: add test to filter-box component verify if render filter options

--- src/components/Filter/FilterBox/FilterBox.spec.tsx
+++ src/components/Filter/FilterBox/FilterBox.spec.tsx
@@ -1,31 +1,37 @@
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { useState } from "react";
+import { HTMLAttributes, useState } from "react";
 
 interface FilterBoxProps {
   filter: {
     title: string;
+    filters: Array<{ filter: string }>;
   };
 }
+
+const FilterOption = ({ ...rest }: HTMLAttributes<HTMLDivElement>) => {
+  return <div {...rest}></div>;
+};
 
 const FilterBox = ({ filter }: FilterBoxProps) => {
   const [isFilterOptionsOpen, setIsFilterOptionsOpen] = useState(false);
 
   return (
-    <div onClick={() => setIsFilterOptionsOpen(!isFilterOptionsOpen)} data-testid='component'>
+    <div
+      onClick={() => setIsFilterOptionsOpen(!isFilterOptionsOpen)}
+      data-testid="component"
+    >
       <span>{filter.title}</span>
       {isFilterOptionsOpen ? (
-        <div
-          data-testid="arrow-to-close-options"
-          
-        >
-          arrow up
-        </div>
+        <div data-testid="arrow-to-close-options">arrow up</div>
       ) : (
-        <div
-          data-testid="arrow-to-open-options"
-        >
-          arrow down
+        <div>
+          <div data-testid="arrow-to-open-options">arrow down</div>
+          <div>
+            {filter.filters.map((filter) => (
+              <FilterOption data-testid="filters-options" />
+            ))}
+          </div>
         </div>
       )}
     </div>
@@ -36,6 +42,7 @@ describe("Filter Box Component", () => {
   it("should have a title passed by props", () => {
     const mockFilterBoxProps = {
       title: "any_title",
+      filters: [],
     };
     const { getByText } = render(<FilterBox filter={mockFilterBoxProps} />);
 
@@ -44,6 +51,7 @@ describe("Filter Box Component", () => {
   it("should have a arrow icon to open filters options", () => {
     const mockFilterBoxProps = {
       title: "any_title",
+      filters: [],
     };
     const { getByTestId } = render(<FilterBox filter={mockFilterBoxProps} />);
 
@@ -52,12 +60,13 @@ describe("Filter Box Component", () => {
   it("should have a arrow icon to open filters options", async () => {
     const mockFilterBoxProps = {
       title: "any_title",
+      filters: [],
     };
     const { getByTestId, queryByTestId } = render(
       <FilterBox filter={mockFilterBoxProps} />
     );
 
-    await userEvent.click(getByTestId('component'));
+    await userEvent.click(getByTestId("component"));
 
     expect(getByTestId("arrow-to-close-options")).toBeInTheDocument();
     expect(queryByTestId("arrow-to-open-options")).toBeFalsy();
@@ -65,14 +74,33 @@ describe("Filter Box Component", () => {
   it("should have a close section filters options", async () => {
     const mockFilterBoxProps = {
       title: "any_title",
+      filters: [],
     };
     const { getByTestId, queryByTestId } = render(
       <FilterBox filter={mockFilterBoxProps} />
     );
 
-    await userEvent.dblClick(getByTestId('component'));
+    await userEvent.dblClick(getByTestId("component"));
 
     expect(getByTestId("arrow-to-open-options")).toBeInTheDocument();
     expect(queryByTestId("arrow-to-close-options")).toBeFalsy();
+  });
+  it("should have a filter options", () => {
+    const mockFilterBoxProps = {
+      title: "any_title",
+      filters: [
+        { filter: "any_filter_1" },
+        { filter: "any_filter_2" },
+        { filter: "any_filter_3" },
+      ],
+    };
+    const { getAllByTestId } = render(
+      <FilterBox filter={mockFilterBoxProps} />
+    );
+
+    getAllByTestId("filters-options").map((filterOption) =>
+      expect(filterOption).toBeInTheDocument()
+    );
+    expect(getAllByTestId("filters-options").length).toBe(3);
   });
 });
