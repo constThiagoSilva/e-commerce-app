4b9f1cd26c143fac519b6ba076a1991f682c4136
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-21 19:47:56 -0300

    refactor: refac "should render the filter options: Size, Price, Category" test

--- src/pages/Products/components/FiltersSection/FiltersSection.tsx
+++ src/pages/Products/components/FiltersSection/FiltersSection.tsx
@@ -1,13 +1,31 @@
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useMemo } from "react";
 import { FilterOption } from "../../../../components/Filter/FilterBox/FilterOptions/FilterOption";
 
 export const FiltersSection = ({ ...rest }: HTMLAttributes<HTMLDivElement>) => {
+  const FILTER_OPTIONS = useMemo(
+    () => [
+      {
+        title: "Size",
+      },
+      {
+        title: "Price",
+      },
+      {
+        title: "Category",
+      },
+    ],
+    []
+  );
+
   return (
     <div {...rest}>
       <section>
-        <FilterOption filterTitle="size" data-testid='filter-options-component'/>
-        <FilterOption filterTitle="price" data-testid='filter-options-component'/>
-        <FilterOption filterTitle="category" data-testid='filter-options-component'/>
+        {FILTER_OPTIONS.map((option) => (
+          <FilterOption
+            filterTitle={option.title}
+            data-testid="filter-options-component"
+          />
+        ))}
       </section>
     </div>
   );
