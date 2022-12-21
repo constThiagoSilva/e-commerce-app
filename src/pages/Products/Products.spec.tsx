9e70841eaf9c2059d9219c56ed16af8901fe90d3
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-21 17:23:27 -0300

    refactor: refac "should have a list of product cards" test and product-page

--- src/pages/Products/Products.spec.tsx
+++ src/pages/Products/Products.spec.tsx
@@ -1,23 +1,90 @@
-import { render } from "@testing-library/react"
-import { ProductCard } from "../../components/ProductCard/ProductCard"
+import { render } from "@testing-library/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ProductCard } from "../../components/ProductCard/ProductCard";
+import { Product } from "../../interfaces/Product";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 const Products = () => {
-    return (
-        <div>
-            <ProductCard product={{category: 'any', image_url: 'any',inPromotion: null, price: 100, title: 'any'}} data-testid='product-card-component'/>
-            <ProductCard product={{category: 'any', image_url: 'any',inPromotion: null, price: 100, title: 'any'}} data-testid='product-card-component'/>
-            <ProductCard product={{category: 'any', image_url: 'any',inPromotion: null, price: 100, title: 'any'}} data-testid='product-card-component'/>
-            <ProductCard product={{category: 'any', image_url: 'any',inPromotion: null, price: 100, title: 'any'}} data-testid='product-card-component'/>
-            <ProductCard product={{category: 'any', image_url: 'any',inPromotion: null, price: 100, title: 'any'}} data-testid='product-card-component'/>
-        </div>
-    )
-}
+  const [products, setProducts] = useState<Product[]>([]);
 
-describe('Products Page', () => {
-    it('should have a list of product cards', () => {
-        const {getAllByTestId} = render(<Products/>)
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      const { data } = await axios.get("../../data/products.json");
 
-        getAllByTestId(/product-card-component/i).map(element => expect(element).toBeInTheDocument())
-        expect(getAllByTestId(/product-card-component/i).length).toBe(5)
-    })
-})
+      setProducts(data);
+    };
+
+    fetchAllProducts();
+  }, []);
+
+  return (
+    <div>
+      {products.map((product) => (
+        <ProductCard
+          product={{
+            category: "any",
+            image_url: "any",
+            inPromotion: null,
+            price: 100,
+            title: "any",
+          }}
+          data-testid="product-card-component"
+        />
+      ))}
+    </div>
+  );
+};
+
+describe("Products Page", () => {
+  it("should have a list of product cards", async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: [
+        {
+          category: "any_category",
+          image_url: "any_url",
+          inPromotion: null,
+          price: 100,
+          title: "any_title",
+        },
+        {
+          category: "any_category",
+          image_url: "any_url",
+          inPromotion: null,
+          price: 100,
+          title: "any_title",
+        },
+        {
+          category: "any_category",
+          image_url: "any_url",
+          inPromotion: null,
+          price: 100,
+          title: "any_title",
+        },
+        {
+          category: "any_category",
+          image_url: "any_url",
+          inPromotion: null,
+          price: 100,
+          title: "any_title",
+        },
+        {
+          category: "any_category",
+          image_url: "any_url",
+          inPromotion: null,
+          price: 100,
+          title: "any_title",
+        },
+      ],
+    });
+
+    const { findAllByTestId } = render(<Products />);
+
+    (await findAllByTestId(/product-card-component/i)).map((element) =>
+      expect(element).toBeInTheDocument()
+    );
+    expect((await findAllByTestId(/product-card-component/i)).length).toBe(5);
+  });
+});
