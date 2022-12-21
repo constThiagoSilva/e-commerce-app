3b3ebbc724fb4f9a007f65107cd0029f8af39a78
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-21 17:42:27 -0300

    feat: add test in products-page to assert if when area to open filter-section was clicked, open the filters section

--- src/pages/Products/Products.spec.tsx
+++ src/pages/Products/Products.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { HTMLAttributes, useEffect, useState } from "react";
 import { ProductCard } from "../../components/ProductCard/ProductCard";
 import { Product } from "../../interfaces/Product";
 import { makeMockAxiosReturnedValue } from "./factories/mocks/makeMockedAxiosReturnValue";
@@ -8,8 +9,13 @@ import { makeMockAxiosReturnedValue } from "./factories/mocks/makeMockedAxiosRet
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+const FiltersSection = ({ ...rest }: HTMLAttributes<HTMLDivElement>) => {
+  return <div {...rest}>filter section</div>;
+};
+
 const Products = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [isFiltersSectionOpen, setIsFiltersSectionOpen] = useState(false);
 
   useEffect(() => {
     const fetchAllProducts = async () => {
@@ -23,12 +29,15 @@ const Products = () => {
 
   return (
     <div>
-        <section>
-            <div data-testid='open-or-close-filter'>
-                <span>filtros</span>
-                <span>icon</span>
-            </div>
-        </section>
+      <section>
+        <div
+          data-testid="open-or-close-filter"
+          onClick={() => setIsFiltersSectionOpen(!isFiltersSectionOpen)}
+        >
+          <span>filtros</span>
+          <span>icon</span>
+        </div>
+      </section>
       {products.map((product) => (
         <ProductCard
           product={{
@@ -41,14 +50,16 @@ const Products = () => {
           data-testid="product-card-component"
         />
       ))}
+      {isFiltersSectionOpen && (
+        <FiltersSection data-testid="filters-section-component" />
+      )}
     </div>
   );
 };
 
-
 describe("Products Page", () => {
   it("should have a list of product cards", async () => {
-    makeMockAxiosReturnedValue(mockedAxios)
+    makeMockAxiosReturnedValue(mockedAxios);
     const { findAllByTestId } = render(<Products />);
 
     (await findAllByTestId(/product-card-component/i)).map((element) =>
@@ -56,10 +67,17 @@ describe("Products Page", () => {
     );
     expect((await findAllByTestId(/product-card-component/i)).length).toBe(5);
   });
-  it('should have a area to open or close filters with title: filter; and an icon', () => {
-    const {getByText, getByTestId} = render(<Products/>)
+  it("should have a area to open or close filters with title: filter; and an icon", () => {
+    const { getByText, getByTestId } = render(<Products />);
 
-    expect(getByTestId(/open-or-close-filter/i)).toBeInTheDocument()
-    expect(getByTestId(/open-or-close-filter/i)).toHaveTextContent(/filtros/i)
-  })
+    expect(getByTestId(/open-or-close-filter/i)).toBeInTheDocument();
+    expect(getByTestId(/open-or-close-filter/i)).toHaveTextContent(/filtros/i);
+  });
+  it("should open the filters section when area to open filter was clicked", async () => {
+    const { getByTestId } = render(<Products />);
+
+    await userEvent.click(getByTestId(/open-or-close-filter/i));
+
+    expect(getByTestId(/filters-section-component/i));
+  });
 });
