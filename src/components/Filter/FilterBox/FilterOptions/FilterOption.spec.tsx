a8d233bb049b57461a4a06f9158a6f2e69b85733
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-30 20:41:58 -0300

    refactor: add mock-filter-option factory to filter-option component test

--- src/components/Filter/FilterBox/FilterOptions/FilterOption.spec.tsx
+++ src/components/Filter/FilterBox/FilterOptions/FilterOption.spec.tsx
@@ -1,39 +1,28 @@
 import { render } from "@testing-library/react"
 import userEvent from '@testing-library/user-event'
+import { makeMockFilterOptionsProps } from "./factories/makeMockFilterOptionsProp"
 import { FilterOption } from "./FilterOption"
 
 describe('Filter Option Component', () => {
     it('should have a title', () => {
-        const mockFilterOptionProps = {
-            filterTitle: 'any_title'
-        }
-        const {getByText} = render(<FilterOption filterTitle={mockFilterOptionProps.filterTitle} />)
+        const {getByText} = render(<FilterOption filterTitle={makeMockFilterOptionsProps().filterTitle} />)
 
         expect(getByText('any_title')).toBeInTheDocument()
     })
     it('should have a checkbox', () => {
-        const mockFilterOptionProps = {
-            filterTitle: 'any_title'
-        }
-        const {getByTestId} = render(<FilterOption filterTitle={mockFilterOptionProps.filterTitle}/>)
+        const {getByTestId} = render(<FilterOption filterTitle={makeMockFilterOptionsProps().filterTitle}/>)
 
         expect(getByTestId('checkbox-element')).toBeInTheDocument()
     })
     it('should have a checkbox when component is click', async () => {
-        const mockFilterOptionProps = {
-            filterTitle: 'any_title'
-        }
-        const {getByTestId} = render(<FilterOption filterTitle={mockFilterOptionProps.filterTitle}/>)
+        const {getByTestId} = render(<FilterOption filterTitle={makeMockFilterOptionsProps().filterTitle}/>)
 
         await userEvent.click(getByTestId('checkbox-element'))
 
         expect(getByTestId('check')).toBeInTheDocument()
     })
     it('not should have a checkbox when comppnent is twice clicked', async () => {
-        const mockFilterOptionProps = {
-            filterTitle: 'any_title'
-        }
-        const {getByTestId, queryByTestId} = render(<FilterOption filterTitle={mockFilterOptionProps.filterTitle}/>)
+        const {getByTestId, queryByTestId} = render(<FilterOption filterTitle={makeMockFilterOptionsProps().filterTitle}/>)
 
         await userEvent.dblClick(getByTestId('checkbox-element'))
 
