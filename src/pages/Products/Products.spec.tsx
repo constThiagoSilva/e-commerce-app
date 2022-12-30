6263139f72fb509cede6ee03ff44b7fd72f1ab1d
drakeira <thiago.p.dasilva2005@gmail.com>
2022-12-30 19:10:33 -0300

    fix: fix act warning in products test

--- src/pages/Products/Products.spec.tsx
+++ src/pages/Products/Products.spec.tsx
@@ -1,8 +1,9 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import axios from "axios";
 import { Products } from "./Products";
 import { makeMockAxiosReturnedValue } from "./factories/mocks/makeMockedAxiosReturnValue";
+import { act } from "react-dom/test-utils";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -11,37 +12,47 @@ describe("Products Page", () => {
   describe("Products Section", () => {
     it("should have a list of product cards", async () => {
       makeMockAxiosReturnedValue(mockedAxios);
-      const { findAllByTestId } = render(<Products />);
 
-      (await findAllByTestId(/product-card-component/i)).map((element) =>
+      await act(async () => {
+        const {} = render(<Products />);
+      });
+
+      (await screen.findAllByTestId(/product-card-component/i)).map((element) =>
         expect(element).toBeInTheDocument()
       );
-      expect((await findAllByTestId(/product-card-component/i)).length).toBe(5);
+      expect((await screen.findAllByTestId(/product-card-component/i)).length).toBe(5);
     });
   });
 
   describe("Filters Section", () => {
-    it("should have a area to open or close filters with title: filter; and an icon", () => {
-      const { getByTestId } = render(<Products />);
+    it("should have a area to open or close filters with title: filter; and an icon", async () => {
+      await act(async () => {
+        const { } = render(<Products />);
+      });
 
-      expect(getByTestId(/open-or-close-filter/i)).toBeInTheDocument();
-      expect(getByTestId(/open-or-close-filter/i)).toHaveTextContent(
+      expect(screen.getByTestId(/open-or-close-filter/i)).toBeInTheDocument();
+      expect(screen.getByTestId(/open-or-close-filter/i)).toHaveTextContent(
         /filtros/i
       );
     });
     it("should open the filters section when area to open filter was clicked", async () => {
-      const { getByTestId } = render(<Products />);
+      await act(async () => {
+        const { } = render(<Products />);
+      })
 
-      await userEvent.click(getByTestId(/open-or-close-filter/i));
+      await userEvent.click(screen.getByTestId(/open-or-close-filter/i));
 
-      expect(getByTestId(/filters-section-component/i)).toBeInTheDocument();
+      expect(screen.getByTestId(/filters-section-component/i)).toBeInTheDocument();
     });
     it("should close the filters section when area to open filter was clicked twice", async () => {
-      const { getByTestId, queryByTestId } = render(<Products />);
+      await act(async () => {
+        const { } = render(<Products />);
+      })
 
-      await userEvent.dblClick(getByTestId(/open-or-close-filter/i));
 
-      expect(queryByTestId(/filters-section-component/i)).toBeFalsy();
+      await userEvent.dblClick(screen.getByTestId(/open-or-close-filter/i));
+
+      expect(screen.queryByTestId(/filters-section-component/i)).toBeFalsy();
     });
   });
 });
