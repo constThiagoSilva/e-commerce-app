65c03e57c4a05ab11b968c38cb0e9b0e828d3b69
drakeira <thiago.p.dasilva2005@gmail.com>
2023-01-02 15:12:44 -0300

    feat: add filters-selected state in product context

--- src/contexts/ProductContext.tsx
+++ src/contexts/ProductContext.tsx
@@ -4,6 +4,10 @@ import { Product } from "../interfaces/Product";
 export interface IProductContext {
   listOfCurrentProducts: Product[];
   setListOfCurrentProducts: React.Dispatch<SetStateAction<Product[]>>;
+  filterListAccordingFilterProps: { filterName: string; filterValue: string }[];
+  setFilterListAccordingFilterProps: React.Dispatch<
+    SetStateAction<{ filterName: string; filterValue: string }[]>
+  >;
 }
 
 export const ProductContext = createContext<IProductContext | null>(null);
@@ -12,10 +16,17 @@ export const ProductProvider = ({ children }: { children: ReactNode }) => {
   const [listOfCurrentProducts, setListOfCurrentProducts] = useState<Product[]>(
     []
   );
+  const [filterListAccordingFilterProps, setFilterListAccordingFilterProps] =
+    useState<{ filterName: string; filterValue: string }[]>([]);
 
   return (
     <ProductContext.Provider
-      value={{ listOfCurrentProducts, setListOfCurrentProducts }}
+      value={{
+        listOfCurrentProducts,
+        setListOfCurrentProducts,
+        filterListAccordingFilterProps,
+        setFilterListAccordingFilterProps,
+      }}
     >
       {children}
     </ProductContext.Provider>
